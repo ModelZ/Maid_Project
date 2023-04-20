675dc659716e2f757ac39b9a5e9b2bf03ff43ef3
ModelZ <model5053@gmail.com>
2023-04-20 09:39:18 +0700

    Add opencv test

---
+++ opencvvscode/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Microsoft Visual Studio/Maid_Project/opencvvscode
+# Build directory: E:/Microsoft Visual Studio/Maid_Project/opencvvscode/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
